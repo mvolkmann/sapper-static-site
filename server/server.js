854c2d6ed119a30707609404151d103487ce8fe8
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-10-11 17:33:31 +0200

    added server directory

---
+++ server/server.js
@@ -0,0 +1,59 @@
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let lastId = 0;
+const dogs = {};
+
+function mustExist(res, id) {
+  const dog = dogs[id];
+  if (!dog) res.status(404).send(`no dog with id ${id} found`);
+  return dog;
+}
+
+const sendJson = (res, obj) => res.send(JSON.stringify(obj));
+
+app.post('/', (req, res) => {
+  const { breed, name } = req.body;
+  const dog = { id: ++lastId, breed, name };
+  dogs[dog.id] = dog;
+  console.log('server.js post: dogs =', dogs);
+  sendJson(res, dog);
+});
+
+app.get('/', (req, res) => {
+  console.log('server.js get: dogs =', dogs);
+  sendJson(res, dogs);
+});
+
+app.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const dog = mustExist(res, id);
+  if (dog) sendJson(res, dog);
+});
+
+app.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { breed, name } = req.body;
+  if (mustExist(res, id)) {
+    const dog = { breed, id, name };
+    dogs[id] = dog;
+    console.log('server.js put: dogs =', dogs);
+    sendJson(res, dog);
+  }
+});
+
+app.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  if (mustExist(res, id)) {
+    delete dogs[id];
+    console.log('server.js delete: dogs =', dogs);
+    res.send();
+  }
+});
+
+const PORT = 1919;
+app.listen(PORT, () => console.log('listening on', PORT));
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,29 @@
+<script>
+const DONUT_URL = 'http://donut-api.mit.edu/api/';
+
+let donuts = [];
+
+async function getDonuts() {
+  try {
+    const res = await fetch(DONUT_URL + 'list');
+    const list = await res.json();
+  } catch (e) {
+    console.error('getDonuts error:', e);
+  }
+}
+
+
+</script>
+
 <svelte:head>
-	<title>About</title>
+	<title>Donuts</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<form on:submit|preventDefault>
+  <button on:click={getDonuts}>Get Donuts</button>
 
-<p>This is the 'about' page. There's not much here.</p>
+  <h2>Donuts</h2>
+  {#each donuts as donut}
+    <div>{donut}</div>
+  {/each}
+</form>
